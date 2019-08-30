d689b6cce8c11f6cea2880b518e413b5b21d2bfd
Gabriel Goethe <gabo5890@gmail.com>
2019-08-30 14:52:13 -0500

    adeed new dependecies and app.js updated

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.set('view engine', 'js');
+app.set('src', path.join(__dirname, 'src'));
+app.set('view engine', 'ejs');
 
 app.use(express.static('./src'));
 
-app.listen(process.env.PORT || 3000, () => {
+app.set('port', process.env.PORT || 3000 );
+
+app.listen(app.get('port'), () => {
     console.log("running server...")
-})
+});
+
+const cool = require('cool-ascii-faces')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'src')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('src/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
