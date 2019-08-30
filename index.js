47082bb580a24e1e9899548dda1d1e97e6e760ac
Gabriel Goethe <gabo5890@gmail.com>
2019-08-30 15:58:59 -0500

    defined node version and gitignore

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 express()
   .use(express.static(path.join(__dirname, 'src')))
   .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
+  .set('view engine', 'node')
   .get('/', (req, res) => res.sendFile(path.join(__dirname, './src/index.html')))
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
