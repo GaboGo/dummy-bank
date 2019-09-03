36cdba46862dd667799959a0d8ee66273407b5ac
Gabriel Goethe <gabo5890@gmail.com>
2019-09-03 18:20:30 -0500

    added setinterval to keep server up

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const http = require("http")
+
+function keepAwake(){
+  setInterval(function() {
+      http.get("http://gabrielgoethe.herokuapp.com");
+      http.get("http://ggoethe-bank.herokuapp.com");
+      http.get("http://goethe-task-manager.herokuapp.com");
+      console.log("keepAwake heroku apps")
+  }, 300000); // every 5 minutes (300000)
+}
+
+keepAwake();
 
 express()
   .use(express.static(path.join(__dirname, 'src')))
